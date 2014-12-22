1fc06fce168bbceabe4c72cef4aebe8003ea66f4
Daniel Akiva <dakiva@gmail.com>
2014-12-21 22:20:57 -0800

    Added support for dropping a schema from the migrate commandline.

--- migrate-cmd/migrate.go
+++ migrate-cmd/migrate.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/dakiva/dbx"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
@@ -12,24 +13,37 @@ func main() {
 	schema := flag.String("schema", "", "Schema name to migrate.")
 	password := flag.String("password", "", "Schema role password to set when creating the schema role.")
 	dsn := flag.String("dsn", "", "Postgres data source name parameters.")
+	dropSchema := flag.Bool("drop", false, "Drops the specified schema.")
 
 	flag.Parse()
-
-	if *migrationsDir == "" {
-		log.Fatalln("A valid migrations directory is required.")
+	if *dsn == "" {
+		log.Fatalln("A valid postgres dsn is required.")
 	}
 	if *schema == "" {
 		log.Fatalln("A valid schema name is required.")
 	}
-	if *password == "" {
-		log.Fatalln("A non-empty password is required.")
+
+	if !*dropSchema {
+		if *migrationsDir == "" {
+			log.Fatalln("A valid migrations directory is required.")
+		}
+		if *password == "" {
+			log.Fatalln("A non-empty password is required.")
+		}
+		db, err := dbx.InitializeDB(*dsn, *schema, *password, *migrationsDir)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		db.Close()
+	} else {
+		db, err := sqlx.Connect("postgres", *dsn)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer db.Close()
+		err = dbx.DropSchema(*schema, db)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
-	if *dsn == "" {
-		log.Fatalln("A valid postgres dsn is required.")
-	}
-	db, err := dbx.InitializeDB(*dsn, *schema, *password, *migrationsDir)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	db.Close()
 }
