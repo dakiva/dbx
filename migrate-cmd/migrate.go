e51a142108872b515695dc671224d290cac93eee
Daniel Akiva <dakiva@gmail.com>
2015-09-26 22:58:57 -0700

    Updated migrate-cmd to automatically use the POSTGRES_DSN if set. Password is now an optional parameter.

--- migrate-cmd/migrate.go
+++ migrate-cmd/migrate.go
@@ -3,20 +3,32 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/dakiva/dbx"
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	postgresDsn = "POSTGRES_DSN"
+)
+
 func main() {
+	pgdsn := os.Getenv(postgresDsn)
 	migrationsDir := flag.String("migrations", "", "Path to migration scripts directory.")
 	schema := flag.String("schema", "", "Schema name to migrate.")
 	password := flag.String("password", "", "Schema role password to set when creating the schema role.")
-	dsn := flag.String("dsn", "", "Postgres data source name parameters.")
+	dsn := flag.String("dsn", "", "Postgres data source name parameters. Can also be specified using the POSTGRES_DSN environment variable.")
 	dropSchema := flag.Bool("drop", false, "Drops the specified schema.")
 
 	flag.Parse()
-	if *dsn == "" {
+
+	if *dsn != "" {
+		// use the dsn parameter value as an override if supplied
+		pgdsn = *dsn
+	}
+	if pgdsn == "" {
+		// if both the env and dsn parameter are not present, return an error.
 		log.Fatalln("A valid postgres dsn is required.")
 	}
 	if *schema == "" {
@@ -27,10 +39,7 @@ func main() {
 		if *migrationsDir == "" {
 			log.Fatalln("A valid migrations directory is required.")
 		}
-		if *password == "" {
-			log.Fatalln("A non-empty password is required.")
-		}
-		db, err := dbx.InitializeDB(*dsn, *schema, *password, *migrationsDir)
+		db, err := dbx.InitializeDB(pgdsn, *schema, *password, *migrationsDir)
 		if err != nil {
 			log.Fatalln(err)
 		}
